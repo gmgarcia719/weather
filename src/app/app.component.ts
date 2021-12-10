d0999b29a2fd404984d642dfc9eac78cc23015e8
Max Garcia <gmgarcia719@gmail.com>
2021-12-10 10:59:11 -0600

    -connected to open weather api. -hardcoded the values for my location will add the abiltly to change later. -used material and flex-layout for a little styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ConfigService } from './weather-config/config.service';
+import { Weather } from './weather-config/weather';
+import citiesJSON from '../assets/city.list.json';
+import { CityList } from './weather-config/city-list';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'weather';
+  weather!: Weather;
+  cities: string[] = [];
+  apiSubscription: Subscription = new Subscription();
+
+  constructor(private configService: ConfigService) {}
+
+  ngOnInit() {
+    this.configService.weatherSubject.subscribe((data: Weather) => {
+      this.weather = data;
+    });
+    this.configService.getWeatherAPI();
+    // this.apiSubscription = this.configService.weatherSubject.subscribe(
+    //   (fetchedWeather: Weather) => {
+    //     this.weather = fetchedWeather;
+    //     console.log(this.weather.weather);
+    //   }
+    // );
+  }
+  ngOnDestroy(): void {
+    this.apiSubscription.unsubscribe();
+  }
 }
